c456e4670d603f99eb829edf401be09af1d56628
TutorialSpiller <mdjonesonly310@gmail.com>
2020-01-26 12:59:17 -0800

    Post api Updated

--- routes/api/post.js
+++ routes/api/post.js
@@ -1,10 +1,29 @@
 const express=require('express');
 const router = express.Router();
+const {check, validationResult} = require('express-validator/check');
+const auth = require('../../middleware/auth');
+const User = require('../../models/User');
+const Profile = require('../../models/Profile');
+const Post = require('../../models/Post');
 
 
-// GET api/post
-// test route
-//public access
-router.get('/',(req,res)=>res.send('post works'))
+// Post api/post
+// create a post route
+//private access
+router.post('/',[auth,[
+    check('text','Text is required').not().isEmpty()
+]],
+async (req,res)=>{
+    const errors = validationResult(req);
+    if(!errors.isEmpty()){
+        return res.status(400).json({ errors:errors.array()})
+    }
+    const user = await (await User.findById(req.user.id)).select('-password');
+
+    const newPost = {
+        text: req.body.text,
+
+    }
+});
 
 module.exports=router;
