ed253f237e14321a43dd3d2d45a256cd2efff930
TutorialSpiller <mdjonesonly310@gmail.com>
2020-03-17 20:52:31 -0700

    Posts reducer, action , and Component built

--- client/src/components/routing/Routes.js
+++ client/src/components/routing/Routes.js
@@ -10,7 +10,7 @@ import AddExperience from "../profile-form/AddExperience";
 import AddEducation from "../profile-form/AddEducation";
 import Profiles from "../profiles/Profiles";
 import Profile from "../profile/Profile";
-// import Posts from "../posts/Posts";
+import Posts from "../posts/Posts";
 // import Post from "../post/Post";
 // import NotFound from "../layout/NotFound";
 import PrivateRoute from "../routing/PrivateRoute";
@@ -30,8 +30,8 @@ const Routes = () => {
 
         <PrivateRoute exact path="/add-experience" component={AddExperience} />
         <PrivateRoute exact path="/add-education" component={AddEducation} />
-        {/* <PrivateRoute exact path="/posts" component={Posts} />
-        <PrivateRoute exact path="/posts/:id" component={Post} />
+        <PrivateRoute exact path="/posts" component={Posts} />
+        {/* <PrivateRoute exact path="/posts/:id" component={Post} />
         <Route component={NotFound} /> */}
       </Switch>
     </section>
