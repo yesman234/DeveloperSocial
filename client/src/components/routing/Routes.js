6d2615725c316dd1417c8ab55353d616b0f1ffb4
TutorialSpiller <mdjonesonly310@gmail.com>
2020-03-22 22:47:34 -0700

    Not found routes 404 implemented

--- client/src/components/routing/Routes.js
+++ client/src/components/routing/Routes.js
@@ -12,7 +12,7 @@ import Profiles from "../profiles/Profiles";
 import Profile from "../profile/Profile";
 import Posts from "../posts/Posts";
 import Post from "../post/Post";
-// import NotFound from "../layout/NotFound";
+import NotFound from "../layout/NotFound";
 import PrivateRoute from "../routing/PrivateRoute";
 
 const Routes = () => {
@@ -32,7 +32,7 @@ const Routes = () => {
         <PrivateRoute exact path="/add-education" component={AddEducation} />
         <PrivateRoute exact path="/posts" component={Posts} />
         <PrivateRoute exact path="/posts/:id" component={Post} />
-        {/* <Route component={NotFound} /> */}
+        <Route component={NotFound} />
       </Switch>
     </section>
   );
