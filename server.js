1dab03f313a1047eadea675070a5f28783ff28cf
Marcellus Devon Jones <mdjonesonly310@gmail.com>
2021-04-16 18:43:14 -0700

    testing

--- server.js
+++ server.js
@@ -6,6 +6,8 @@ const path = require("path");
 
 connectDB();
 
+
+
 app.use(express.json({ extended: false }));
 
 app.get('/',(req,res)=>res.send('api running'))
@@ -15,13 +17,13 @@ app.use("/api/profile", require("./routes/api/profile"));
 app.use("/api/posts", require("./routes/api/post"));
 
 //serve static assets in production
-// if (process.env.NODE_ENV === "default") {
-//   //Set static folder
-//   app.use(express.static("client/build"));
-//   app.get("*", (req, res) => {
-//     res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
-//   });
-// }
+if (process.env.NODE_ENV === "production") {
+  //Set static folder
+  app.use(express.static("client/build"));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
